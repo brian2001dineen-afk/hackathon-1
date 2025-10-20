d62388e31c252e86bbbc2a0c097e9ea4f727d54b
SParb <ryuchigo@gmail.com>
2025-10-20 10:51:41 +0100

    made the car an object that can be created multiple times

--- assets/js/SParb.js
+++ assets/js/SParb.js
@@ -6,6 +6,7 @@ const canvasHeight = canvas.height;
 const playerSize = 40; // Size of Square
 const playerStep = 1; // Speed of square
 const playerKeys = {};
+let cars = [];
 
 // Square starting cooridinates
 let x = (canvasWidth / 2) - (playerSize / 2);  // place player start in middle of screen
@@ -21,7 +22,7 @@ let carDirection = 1; // 1 = right, -1 = left
 
 console.log(canvas);
 
-/** Draw the square */ 
+/** Draw the player controlled object */ 
 function drawSquare() {
     c.fillStyle = "blue";
     c.clearRect(0, 0, canvasWidth, canvasHeight);
@@ -29,7 +30,7 @@ function drawSquare() {
 }
 
 /** Update square position based on keys pressed */
-function updateSquarePosition() {
+function updateSquarePosition(car) {
     if (playerKeys["ArrowUp"]) y -= playerStep;
     if (playerKeys["ArrowDown"]) y += playerStep;
     if (playerKeys["ArrowLeft"]) x -= playerStep;
@@ -38,14 +39,14 @@ function updateSquarePosition() {
     x = Math.min(Math.max(x, 0), canvasWidth - playerSize);
     y = Math.min(Math.max(y, 0), canvasHeight - playerSize);
     // Update car position
-    carX += carSpeed * carDirection;
+    car.x += car.speed * car.direction;
     // Bounce off left/right edges
-    if (carX <= 0 + 30 || carX >= canvasWidth - carWidth - 30) {
-        carDirection *= -1;
+    if (car.x <= 0 + 30 || car.x >= canvasWidth - car.width - 30) {
+        car.direction *= -1;
     }
     drawSquare();
-    drawCar();
-    requestAnimationFrame(updateSquarePosition);
+    drawCar(car1);
+    requestAnimationFrame(() => updateSquarePosition(car));
 }
 
 //Listen for user key presses
@@ -57,12 +58,27 @@ document.addEventListener("keyup", (event) => {
     playerKeys[event.key] = false;
 });
 
-function drawCar() {
-    c.fillStyle = "green"; // Rectangle color
-    c.fillRect(carX, carY, carWidth, carHeight); // x, y, width, height
+function drawCar(car) {
+    c.fillStyle = car.colour; // Rectangle color
+    c.fillRect(car.x, car.y, car.width, car.height); // x, y, width, height
 };
+
+/** Create a car object */
+function createCar(x, y, width, height, speed, colour) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.speed = speed;
+    this.colour = colour;
+    this.direction = 1;
+};
+
+const car1 = new createCar(50, 200, 60, 40, 2, "red")
+console.log(car1.colour);
+
 
 // Start loop
 drawSquare();
-drawCar();
-updateSquarePosition();
+drawCar(car1);
+updateSquarePosition(car1);
