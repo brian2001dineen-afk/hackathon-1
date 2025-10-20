1fe68aef066543eafb5837c955e567047cef5420
SParb <ryuchigo@gmail.com>
2025-10-20 10:05:26 +0100

    change red circle into green sqaure

--- assets/js/SParb.js
+++ assets/js/SParb.js
@@ -1,68 +1,68 @@
 const canvas = document.querySelector("canvas");
 let c = canvas.getContext("2d");
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+
+const playerSize = 40; // Size of Square
+const playerStep = 1; // Speed of square
+const playerKeys = {};
 
 // Square starting cooridinates
-let x = 100;
-let y = 100;
+let x = (canvasWidth / 2) - (playerSize / 2);  // place player start in middle of screen
+let y = canvasHeight - playerSize - 10; // place player start at bottom of screen
 
-const size = 40; // Size of Square
-const step = 1; // Speed of square
-const keys = {};
-
-// Circle starting coordinates
-let circleX = 50;
-let circleY = 200;
-let circleRadius = 1;
-let circleSpeed = 2;
-let circleDirection = 1; // 1 = right, -1 = left
+// Car starting coordinates
+let carX = 50;
+let carY = 200;
+let carWidth = 60;
+let carHeight = 40;
+let carSpeed = 2;
+let carDirection = 1; // 1 = right, -1 = left
 
 console.log(canvas);
 
 /** Draw the square */ 
 function drawSquare() {
     c.fillStyle = "blue";
-    c.clearRect(0, 0, canvas.width, canvas.height);
-    c.fillRect(Math.round(x), Math.round(y), size, size);
+    c.clearRect(0, 0, canvasWidth, canvasHeight);
+    c.fillRect(Math.round(x), Math.round(y), playerSize, playerSize);
 }
 
 /** Update square position based on keys pressed */
 function updateSquarePosition() {
-    if (keys["ArrowUp"]) y -= step;
-    if (keys["ArrowDown"]) y += step;
-    if (keys["ArrowLeft"]) x -= step;
-    if (keys["ArrowRight"]) x += step;
+    if (playerKeys["ArrowUp"]) y -= playerStep;
+    if (playerKeys["ArrowDown"]) y += playerStep;
+    if (playerKeys["ArrowLeft"]) x -= playerStep;
+    if (playerKeys["ArrowRight"]) x += playerStep;
     // Clamp position to stay within canvas when step isnt divisible by the canvas dimentions
-    x = Math.min(Math.max(x, 0), canvas.width - size);
-    y = Math.min(Math.max(y, 0), canvas.height - size);
-    // Update circle position
-    circleX += circleSpeed * circleDirection;
+    x = Math.min(Math.max(x, 0), canvasWidth - playerSize);
+    y = Math.min(Math.max(y, 0), canvasHeight - playerSize);
+    // Update car position
+    carX += carSpeed * carDirection;
     // Bounce off left/right edges
-    if (circleX - circleRadius <= 50 || circleX + circleRadius >= canvas.width -50) {
-        circleDirection *= -1;
+    if (carX <= 0 + 30 || carX >= canvasWidth - carWidth - 30) {
+        carDirection *= -1;
     }
     drawSquare();
-    drawCircle();
+    drawCar();
     requestAnimationFrame(updateSquarePosition);
 }
 
 //Listen for user key presses
 document.addEventListener("keydown", (event) => {
-    keys[event.key] = true;
+    playerKeys[event.key] = true;
 });
 //Listen for key being released
 document.addEventListener("keyup", (event) => {
-    keys[event.key] = false;
+    playerKeys[event.key] = false;
 });
 
-function drawCircle() {
-    c.beginPath();
-    c.arc(circleX, circleY, circleRadius, 0, 2 * Math.PI);
-    c.lineWidth = 30;
-    c.strokeStyle = "red";
-    c.stroke();
+function drawCar() {
+    c.fillStyle = "green"; // Rectangle color
+    c.fillRect(carX, carY, carWidth, carHeight); // x, y, width, height
 };
 
 // Start loop
 drawSquare();
-drawCircle();
+drawCar();
 updateSquarePosition();
