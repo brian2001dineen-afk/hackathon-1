090469db6db33c2647f30e6e7daad5bdb705a631
SParb <ryuchigo@gmail.com>
2025-10-23 11:53:17 +0100

    add back to menu link

--- assets/js/cargametimer.js
+++ assets/js/cargametimer.js
@@ -1,45 +1,47 @@
-// Manages everything based on time (seconds)
+function carTimer() {
+    // Manages everything based on time (seconds)
+    // Pauses time and sounds when the user is tabbed out of the window
+    document.addEventListener("visibilitychange", () => {
+        if (document.hidden) {
+            timer.stop();
+            trafficSound.pause();
+        } else if (!game.player.hit && !game.menu) {
+            timer.start();
+            trafficSound.play();
+        }
+    });
+    const timer = {
+        time: 0,
+        roadScrollSpeed: 1, //Starting speed which the roads scroll downwards
+        roadIncrement: 0.2, //Scrolling speed increase per second
+        speedCap: 8, // Max scroll speed
+        interval: null,
+        /** Start timer counter */
+        start() {
+            this.seconds = 0;
+            this.interval = setInterval(() => {
+                this.time += 0.1;
+            }, 100);
+            if (this.speedInterval) clearInterval(this.speedInterval);
+            this.speedInterval = setInterval(() => {
+                if (this.roadScrollSpeed < this.speedCap - this.roadIncrement) {
+                    //speed cap
+                    this.roadScrollSpeed += this.roadIncrement;
+                }
+            }, 1000);
+        },
+        /** Pause timer counter */
+        stop() {
+            clearInterval(this.interval);
+            clearInterval(this.speedInterval);
+        },
 
-// Pauses time and sounds when the user is tabbed out of the window
-document.addEventListener("visibilitychange", () => {
-    if (document.hidden) {
-        timer.stop();
-        trafficSound.pause();
-    } else if (!game.player.hit && !game.menu) {
-        timer.start();
-        trafficSound.play();
-    }
-});
-const timer = {
-    time: 0,
-    roadScrollSpeed: 1, //Starting speed which the roads scroll downwards
-    roadIncrement: 0.2, //Scrolling speed increase per second
-    speedCap: 8, // Max scroll speed
-    interval: null,
-    /** Start timer counter */
-    start() {
-        this.seconds = 0;
-        this.interval = setInterval(() => {
-            this.time += 0.1;
-        }, 100);
-        if (this.speedInterval) clearInterval(this.speedInterval);
-        this.speedInterval = setInterval(() => {
-            if (this.roadScrollSpeed < this.speedCap - this.roadIncrement) {
-                //speed cap
-                this.roadScrollSpeed += this.roadIncrement;
-            }
-        }, 1000);
-    },
-    /** Pause timer counter */
-    stop() {
-        clearInterval(this.interval);
-        clearInterval(this.speedInterval);
-    },
-
-    /** Reset timer counter */
-    reset() {
-        this.stop();
-        this.time = 0;
-        this.roadScrollSpeed = 1
-    }
-};
+        /** Reset timer counter */
+        reset() {
+            this.stop();
+            this.time = 0;
+            this.roadScrollSpeed = 1;
+        },
+    };
+    return timer;
+}
